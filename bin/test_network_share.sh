9bab33cab2c733f44efac2390b31808b98f50bcc
yinwoods <yinwoods@163.com>
2018-12-10 19:31:30 +0800

    fix test_network_share.sh

--- bin/test_network_share.sh
+++ bin/test_network_share.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-for cid in `docker ps -aq --filter name=busybox --last 2`; do
+for cid in `docker ps -aq --filter name="busybox*" --last 2`; do
     echo "容器 $cid 网络配置："
     docker logs -f $cid
     echo "--------------------"
